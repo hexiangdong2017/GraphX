148014de76864cd772f8cd7331cb8c63f86e6505
HeXiangdong <cloudupliver@hotmail.com>
2020-08-07 20:33:14 +0800

    按照模板创建工程
    dotnet new avalonia.mvvm -o GraphX -n GraphX

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphX.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
